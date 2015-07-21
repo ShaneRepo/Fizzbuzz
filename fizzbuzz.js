24a283679abf56c2db804b128b46e67b6bed5dbc
Shane <shane8641@live.com>
2015-07-21 16:42:46 -0400

    Fizzbuzz 1-100

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,21 @@
 
 var i;
 var myArray = new Array();
-for (i = 1; i < 101; i++)
+for (i = 1; i <= 100; i++)
 {
     myArray[i] = i;
-    console.log(myArray[i]);
+    if (myArray[i] % 3 === 0 && myArray[i] % 5 === 0){
+        document.write("FizzBuzz");
+        document.write("</br>");
+    }else if (myArray[i] % 3 === 0){
+        document.write("Fizz");
+        document.write("</br>");
+    }else if (myArray[i] % 5 === 0){
+        document.write("Buzz");
+        document.write("</br>");
+    }else{
+        document.write(myArray[i]);
+        document.write("</br>");
+    }
+
 }
